a72e82cac945c1f9867096ad62442e778392593c
KyouGenSo <k023g0083@g.neec.ac.jp>
2024-09-30 12:32:36 +0900

    01_02完成

--- main.cpp
+++ main.cpp
@@ -1,7 +1,38 @@
 #include <stdio.h>
 #include <iostream>
 
+// ˆê”Ê“I‚È’À‹à‘ÌŒn
+int RegularPay(int hours) {
+	return 1072 * hours;
+}
+
+// Ä‹A“I‚È’À‹à‘ÌŒn
+int RecursivePay(int hours) {
+	if (hours == 0) {
+		return 0;
+	}
+
+	if (hours == 1) {
+		return 100;
+	} else {
+		return RecursivePay(hours - 1) * 2 - 50;
+	}
+}
+
 int main() {
-	printf("Hello, World!\n");
+	int hours = 8;
+	int regularPay = RegularPay(hours);
+	int recursivePay = RecursivePay(hours);
+
+	while (regularPay >= recursivePay) {
+		hours++;
+		regularPay = RegularPay(hours);
+		recursivePay = RecursivePay(hours);
+	}
+
+	printf("hours: %d\n", hours);
+	printf("regularPay: %d\n", regularPay);
+	printf("recursivePay: %d\n", recursivePay);
+
 	return 0;
 }
